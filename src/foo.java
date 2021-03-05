2bf4a9589063a0466a9ba0fb37d36688004658cb
Evgeny <vasilkalin@gmail.com>
2021-03-05 13:20:06 +0500

    cv xvc c

--- src/foo.java
+++ src/foo.java
@@ -3,14 +3,14 @@
 public class foo {
 
     public static void main(String args[]) {
-
         int n = 5;
         int x = n + 1;
         int y = 1;
         for (int i = 1; i < n; i++) {
             y = y * (x - i);
         }
-
+        
         System.out.print("n! = " + y);
+
     }
 }
